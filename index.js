3edca8cac108f2ee53429bc20acf445a03d1851e
aliev <122043094+alievdavlat@users.noreply.github.com>
2024-11-15 18:32:10 +0500

    test

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Express on test");
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
