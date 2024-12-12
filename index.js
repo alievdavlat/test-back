490ed1ea6dccd5964c849eae250db8029d8871aa
aliev <122043094+alievdavlat@users.noreply.github.com>
2024-12-12 08:56:16 +0500

    ch route

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import router from "./src/routes/routes.js";
 import errorHandler from './src/middleware/errorHandler.js'
 import connectDB from "./src/config/mongoose.config.js";
 import { join } from "path";
+// routes 
+import AuthRouter from "./src/routes/auth.routes.js";
+import CommentsRouter from './src/routes/comments.routes.js'
+import ExperienceRouter from './src/routes/experience.routes.js'
+import GetSelfRouter from './src/routes/getSelf.routes.js'
+import HeroRouter from './src/routes/hero.routes.js'
+import MailRouter from './src/routes/mails.routes.js'
+import ProjectRouter from './src/routes/projects.routes.js'
+
+
 
 // Initialize Express
 const app = express();
@@ -19,8 +28,14 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Welcome to the Express App!');
 });
-
-app.use('/api', router);
+app.use(AuthRouter)
+app.use(CommentsRouter)
+app.use(ExperienceRouter)
+app.use(GetSelfRouter)
+app.use(HeroRouter)
+app.use(MailRouter)
+app.use(ProjectRouter)
+// erro handler
 app.use(errorHandler)
 
 
